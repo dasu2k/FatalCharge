bf26e225aa131cd1e46d56eb12de6d0977f5fa81
dasu2k <tulasidharmulakaluri@gmail.com>
2023-09-01 11:02:10 +0530

    walls
    created wall palettes through which I can place wall prefabs using tile palettes.
    #ruletiles

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -6,14 +6,15 @@ public class PlayerContoller : MonoBehaviour
 {
     // Start is called before the first frame update
     public float sens =3f;
-    public Transform camTransform;
-    private bool isGrounded;
-    public Rigidbody rb;
+    public Camera mainCam;
+
+    public Rigidbody2D rb;
     public float speed = 10f;
     void Start()
     {
         sens = 3f;
-        isGrounded = true;
+        
+        rb = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
@@ -21,26 +22,14 @@ public class PlayerContoller : MonoBehaviour
     {
         //mouse input stuff
         
-        camTransform.Rotate(-sens*(Input.GetAxis("Mouse Y")) , 0 , 0 , Space.Self);
+        mainCam.transform.Rotate(-sens*(Input.GetAxis("Mouse Y")) , 0 , 0 , Space.Self);
         transform.Rotate(0,sens*(Input.GetAxis("Mouse X")),0,Space.Self);
         
         //player movement
          
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * 0.03f,0,Input.GetAxis("Vertical") * 0.03f));
-
-        if(Input.GetButton("Jump") && isGrounded)
-        {
-            isGrounded = false;
-            rb.AddForce(transform.up * 10f);
-        }
-        Debug.Log(isGrounded);
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed,0,Input.GetAxis("Vertical") * speed));
 
     }
-    
-void OnCollisionEnter(Collision collision){
-    Debug.Log("collision!");
-    if(collision.other.tag == "Ground")
-        isGrounded = true;
-}
+
 
 }
