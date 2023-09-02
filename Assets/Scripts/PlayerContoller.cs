e6a218f5724c2fcd7bb43845ea9e4991995e9c37
dasu2k <tulasidharmulakaluri@gmail.com>
2023-09-02 10:27:41 +0530

    player animation
    added head bobbing of player while they are moving and tweaked some settings to improve player movement

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -5,30 +5,34 @@ using UnityEngine;
 public class PlayerContoller : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float sens =3f;
+    public float sens;
     public Camera mainCam;
-
+    public Animator camAnimator;
     public Rigidbody2D rb;
     public float speed = 10f;
     void Start()
     {
         sens = 3f;
-        
         rb = GetComponent<Rigidbody2D>();   
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal") , Input.GetAxis("Vertical"));
         //mouse input stuff
-        
         mainCam.transform.Rotate(-sens*(Input.GetAxis("Mouse Y")) , 0 , 0 , Space.Self);
         transform.Rotate(0,sens*(Input.GetAxis("Mouse X")),0,Space.Self);
         
         //player movement
-         
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed,0,Input.GetAxis("Vertical") * speed));
-
+        if(input != Vector2.zero)
+        {
+            transform.Translate(new Vector3(input.x * speed,0,input.y * speed));
+            camAnimator.SetBool("isMoving" , true);
+        }
+        else{
+            camAnimator.SetBool("isMoving",false);
+        }
     }
 
 
