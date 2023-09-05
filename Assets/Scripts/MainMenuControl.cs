ba8ffac0b50b5380d5417f3fdf76f08f54f6c7f8
dasu2k <tulasidharmulakaluri@gmail.com>
2023-09-06 01:03:21 +0530

    HomeScreen
    -added a homescreen , the player starts up in the homescreen scene.
    -A play button to get the player to sampleScene or the first level ( or any other scene)

---
+++ Assets/Scripts/MainMenuControl.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour
+{
+
+
+    public void onPlayClick(){
+        SceneManager.LoadScene("SampleScene");
+    }
+}
